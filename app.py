dd2b44dc35165e9e7b92ec3d52df11f1eba6587e
MMIX2009 <btshilombo@comcast.net>
2025-02-16 18:58:47 -0800

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from datetime import datetime
 import pytz
 
 API_KEY = "******************************"  # YOUR_GOOGLE_MAPS_API_KEY
-headers = {
-    "authorization": st.secrets["API_KEY"],
-    "content-type": "application/json"
-}
+# headers = {
+#     "authorization": st.secrets["API_KEY"],
+#     "content-type": "application/json"
+# }
 
 
 # City options with coordinates and timezones
